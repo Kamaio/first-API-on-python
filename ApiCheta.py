536ec24aef7c08036a13f57237630f64930a045c
Kamaio <jfocampo173@gmail.com>
2024-09-03 00:22:56 -0500

    modified:   ApiCheta.py

--- ApiCheta.py
+++ ApiCheta.py
@@ -3,7 +3,9 @@ import UI.mostrarDatos
 
 departamento_usuario = input("Bienvenido al programa de consulta de casos de covid\nIngrese el departamento del que desea consultar los registros: ")
 limite = int(input("Ingrese la cantidad de registros que desea consultar: "))
+if(limite < 203):
+    datosPD = API.consumeAPI.consume("departamento_nom='"+departamento_usuario+"'", limite)
 
-datosPD = API.consumeAPI.consume("departamento_nom='"+departamento_usuario+"'", limite)
+    UI.mostrarDatos.printf(datosPD)
 
-UI.mostrarDatos.printf(datosPD)
+else: print()
